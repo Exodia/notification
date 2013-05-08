33034502d76900653a017f4d087d4a312d4d8c3a
Exodia <d_xinxin@163.com>
2013-05-08 21:02:06 +0800

    添加接口

--- 1.0/notification.js
+++ 1.0/notification.js
@@ -15,31 +15,73 @@ KISSY.add(function () {
      * @constructor
      */
     function DesktopNotify(title, options) {
-       this.title = title;
+        this._title = title
+
+        if (!options) {
+            return this
+        }
+
+        this.dir = options.dir
+        this.onclick = options.onclick
+        this.onshow = options.onshow
+        this.onerror = options.onerror
+        this.ondisplay = options.ondisplay
+        this.tag = options.tag
+    }
+
+    /**
+     * 快速显示一个桌面通知
+     * @param title
+     * @param body
+     * @param icon
+     */
+    DesktopNotify.show = function (title, body, icon) {
+
+    }
+
+    /**
+     * 检查权限状态
+     * @return {Number}: 0为允许，1为不允许， 2为禁止
+     */
+    DesktopNotify.checkPermission = function () {
+
+    }
+
+    /**
+     * 检查是否得到授权
+     * @return {Boolean}： true表示得到授权
+     */
+    DesktopNotify.isPermitted = function () {
+        return this.checkPermission() === 0;
+    }
+
+
+    /**
+     * 请求授权
+     * @param cb {function} 得到授权后的回调函数
+     */
+    DesktopNotify.requestPermission = function (cb) {
 
     }
 
 
+    /**
+     * 检测是否支持Notification，支持返回true
+     */
+    DesktopNotify.isSupport = function () {
+        return 'Notification' in window || 'webkitNotifications' in window;
+    }
 
-    var DesktopNotify = {
 
-        /**
-         *检测是否支持Notification，支持返回true
-         */
-        isSupport: function () {
-            return 'Notification' in window || 'webkitNotifications' in window;
-        },
+    DesktopNotify.prototype = {
+
+        constructor: DesktopNotify,
 
         /**
          *弹出一个文本桌面通知
          *
-         * @param {String} iconURL:图标资源
-         * @param {String} title: 标题
-         * @param {String} content: 内容
          */
-        show: function (iconURL, title, content) {
-            _instance = this.create(iconURL, title, content);
-            _instance.show();
+        show: function () {
         },
 
 
@@ -49,8 +91,6 @@ KISSY.add(function () {
          * @param {String} url:html链接资源
          */
         showHTML: function (url) {
-            _instance = this.createHTML(url);
-            _instance.show();
         },
 
         /***
@@ -59,67 +99,9 @@ KISSY.add(function () {
          * @param {Object} cb： 隐藏后的回调函数
          *
          */
-        hide: function (cb) {
-            _instance && _instance.close();
-            cb && cb();
+        close: function (cb) {
         },
 
-        /**
-         * 释放通知对话框引用
-         */
-        destroy: function () {
-            _instance = null,
-                _permissionStatus = -1;
-        },
-
-        /**
-         * 检查权限状态
-         * @return {Number}: 0为允许，1为不允许， 2为禁止
-         */
-        checkPermission: function () {
-            return _permissionStatus = webkitNotifications.checkPermission();
-        },
-
-        /**
-         * 检查是否得到授权
-         * @return {Boolean}： true表示得到授权
-         */
-        isPermitted: function () {
-            return this.checkPermission() === 0;
-        },
-
-
-        /**
-         * 请求授权
-         * @param {Object} cb：得到授权后的回调函数
-         */
-        requestPermission: function (cb) {
-            if (this.isPermitted()) {
-                cb && cb();
-            } else {
-                webkitNotifications.requestPermission(cb);
-            }
-        },
-
-        /**
-         * 创建一个文本性质的通知对话框，但不展示
-         * @param {Object} iconURL
-         * @param {Object} title
-         * @param {Object} content
-         * @return {Object} Notification实例
-         */
-        create: function (iconURL, title, content) {
-            return webkitNotifications.createNotification(iconURL, title, content);
-        },
-
-        /**
-         * 创建一个HTML性质的通知对话框，但不展示
-         * @param {Object} url: 指向html页面的链接
-         * @return {Object} Notification实例
-         */
-        createHTML: function (url) {
-            return webkitNotifications.createHTMLNotification(url);
-        },
 
         /**
          * 添加事件监听函数
@@ -135,7 +117,7 @@ KISSY.add(function () {
          * @param {Object} type: 事件类型
          * @param {Object} fn: 监听函数
          */
-        un: function (type, fn) {
+        off: function (type, fn) {
             _eventTable[type] && _instance && _instance.removeEventListener(type, fn, false);
         }
     };
